5b67be08643bf9807c07157f4b87848684191bde
Jay Davis <jayddavis191@gmail.com>
2020-08-27 01:01:21 +1000

    Prints user's score back to user

--- broken_score.py
+++ broken_score.py
@@ -3,17 +3,24 @@ CP1404 - Jay Davis Practical
 Program to obtain score from user and input their score
 """
 
-def main():
-    number = float(input("Score: "))
-    return number
+def main(number):
+    if number > 100:
+        return "Invalid score"
+    elif number >= 90:
+        return "Excellent"
+    elif number >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
 
+score = float(input("Score: "))
+score_converted = main(score)
+print(score_converted)
+print("Your score was {:,.0f} which is {}.".format(score, score_converted))
 
-
-score = main()
-print("Your score was {:,.0f}.".format(score))
 
 import random
-
-random_number = random.random()
-print("A random number is {:,.2f}.".format(random_number))
+random_score = random.randint(0, 101)
+random_score_converted = main(random_score)
+print("A random score is {:,.0f} which is {}.".format(random_score, random_score_converted))
