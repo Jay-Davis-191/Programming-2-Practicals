214c34dd16b603a74de6ff79197e2fc0bf705cf7
Jay Davis <70142128+Jay-Davis-191@users.noreply.github.com>
2020-10-07 22:37:44 +1000

    Update box_layout_demo.py

--- Practical 7/box_layout_demo.py
+++ Practical 7/box_layout_demo.py
@@ -8,13 +8,12 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
-    def handle_greet(self):
-        print("test")
-        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+    def greet_user(self):
+        self.root.ids.name_label.text = "Hello " + self.root.ids.input_name.text
 
     def clear_everything(self):
         self.root.ids.input_name.text = ""
-        self.root.ids.output_label.text = ""
+        self.root.ids.name_label.text = ""
 
 
 BoxLayoutDemo().run()
