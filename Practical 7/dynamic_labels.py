a733a0077b03871dd936057096c0ad068051c179
Jay Davis <70142128+Jay-Davis-191@users.noreply.github.com>
2020-10-07 23:03:08 +1000

    Update dynamic_labels.py

--- Practical 7/dynamic_labels.py
+++ Practical 7/dynamic_labels.py
@@ -9,7 +9,6 @@ class DynamicWidgetsApp(App):
     status_text = StringProperty()
 
     def __init__(self, **kwargs):
-        """Construct main app."""
         super().__init__(**kwargs)
         self.name_to_phone = {"Adam", "Bryan", "Catherine", "David", "Earl", "Fred"}
 
@@ -20,16 +19,11 @@ class DynamicWidgetsApp(App):
         return self.root
 
     def create_widgets(self):
-        """Create buttons from dictionary entries and add them to the GUI."""
         for name in self.name_to_phone:
-            # create a button for each data entry, specifying the text and id
-            # (although text and id are the same in this case, you should see how this works)
             temp_button = Label(text=name, id=name)
-            # add the button to the "entries_box" layout widget
             self.root.ids.entries_box.add_widget(temp_button)
 
     def clear_all(self):
-        """Clear all of the widgets that are children of the "entries_box" layout widget."""
         self.root.ids.entries_box.clear_widgets()
 
 
