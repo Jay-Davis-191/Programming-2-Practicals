c1a7227345939b074e9931c9bfcfef536569ab6b
Jay Davis <70142128+Jay-Davis-191@users.noreply.github.com>
2020-10-15 23:59:46 +1000

    Update Taxi_test.py

--- Practical 8/Taxi_test.py
+++ Practical 8/Taxi_test.py
@@ -1,1 +1,22 @@
+"""CP1404 Practical 8
+    Taxi_test.py
+    By Jay Davis"""
 
+
+from Taxi import Taxi
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    Car = Taxi("Prius 1", 100)
+    Car.start_fare()
+    Car.drive(40)
+    print(Car.__str__())
+    print("Total fare: $" + "{:.2f}".format(Car.get_fare()))
+    Car.start_fare()
+    Car.drive(100)
+    print(Car.__str__())
+    print("Total fare: $" + "{:.2f}".format(Car.get_fare()))
+
+
+main()
